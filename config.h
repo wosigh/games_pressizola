63a34a18e4989f27fba825f45069600170612cfa
Will Dietz <w@wdtz.org>
2010-09-21 12:23:30 -0500

    Remove build artifcats from version control.

--- config.h
+++
@@ -1,9 +0,0 @@
-/* config.h.  Generated automatically by configure.  */
-/* config.h.in.  Generated automatically from configure.in by autoheader.  */
-
-/* Name of package */
-#define PACKAGE "dossizola"
-
-/* Version number of package */
-#define VERSION "1.0"
-
